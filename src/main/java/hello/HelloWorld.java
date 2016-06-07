10572fc1565fc01a0e52a89bb4e636aa20223a31
Erwan Rouzel <erwan.rouzel@gmail.com>
2016-06-07 17:54:37 +0200

    ApiInfo

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,29 +1,18 @@
 package hello;
 
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Controller
 @EnableAutoConfiguration
 @ComponentScan("hello")
 public class HelloWorld {
 
-    @ApiOperation(value = "getGreeting", nickname = "getGreeting")
-
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success", response = String.class),
-            @ApiResponse(code = 401, message = "Unauthorized"),
-            @ApiResponse(code = 403, message = "Forbidden"),
-            @ApiResponse(code = 404, message = "Not Found"),
-            @ApiResponse(code = 500, message = "Failure")})
+    @ApiOperation(value = "hello", nickname = "hello")
     @RequestMapping(method = RequestMethod.GET, path="/hello", produces = "application/json")
     @ResponseBody
     String hello() {
